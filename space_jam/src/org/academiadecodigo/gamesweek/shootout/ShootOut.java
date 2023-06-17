421f03fea3105e18b0378cbb37159f92aa1083b2
Cláudia <claudia_miranda_costa@hotmail.com>
2023-06-17 21:46:51 +0100

    Set background, hoop and ball picture, size and position.

--- space_jam/src/org/academiadecodigo/gamesweek/shootout/ShootOut.java
+++ space_jam/src/org/academiadecodigo/gamesweek/shootout/ShootOut.java
@@ -1,27 +1,50 @@
 package org.academiadecodigo.gamesweek.shootout;
 
 import org.academiadecodigo.gamesweek.Game;
+import org.academiadecodigo.simplegraphics.graphics.Canvas;
 import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.simplegraphics.graphics.Shape;
 import org.academiadecodigo.simplegraphics.pictures.Picture;
+
+import javax.swing.*;
+import java.util.ArrayList;
 
 public class ShootOut {
 
-    Picture background;
+    private Canvas canvas;
+    private Picture background;
+    private Picture hoop;
+    private Picture ball;
 
-
-    Rectangle rectangle;
+    public ShootOut() {
+        this.canvas = Canvas.getInstance();
+        this.background = new Picture(Game.PADDING,Game.PADDING,"space_jam\\resources\\bleachers.jpeg");
+        this.hoop = new Picture(Game.PADDING,Game.PADDING,"space_jam\\resources\\hoop.png");
+        this.ball = new Picture(Game.PADDING,Game.PADDING,"space_jam\\resources\\ball.png");
+    }
 
     public void init(){
 
-        Picture background = new Picture(Game.PADDING, Game.PADDING, "resources/bleachers.jpeg");
+        // Test to hide objects that are outside the canvas bounds. NOT WORKING!!
+        //JFrame frame = new JFrame("FrameDemo");
+        //frame.pack();
+        //frame.setVisible(true);
+        Rectangle shape = new Rectangle(Game.PADDING, Game.PADDING, 1250, 750);
+        canvas.show(shape);
+
+        // Reposition, resize and show background image (change 1250 and 750 to Game.getWidth() and Game.getHeight())
+        background.translate((double) (1250 - background.getWidth()) / 2, (double) (750 - background.getHeight()) / 2);
+        background.grow((double) (1250 - background.getWidth()) / 2, (double) (750 - background.getHeight()) / 2);
         background.draw();
-        Picture hoop = new Picture(10,10,"resources/hoop.png");
-        //hoop.grow(-2000,-2000);
-        //hoop.draw();
-        Picture ball = new Picture(10,10,"resources/ball.png");
-        //ball.grow(-380,-380);
-        ball.draw();
-        //ball.translate(-150,-100);
+
+        // Center and draw hoop (change 1250 and 750 to Game.getWidth() and Game.getHeight())
+        hoop.translate((double) (1250 - hoop.getWidth()) / 2, (double) (750 - hoop.getHeight()) / 2);
+        hoop.draw();
+
+        // Reposition, resize and show background image
+        ball.translate((double) 1250 / 2,500);
+        // ball.grow(-120,-120);
+        canvas.show(ball);
 
 
 
