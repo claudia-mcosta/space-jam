434badc04969111968fc6a0d1f59398d5dd78637
Luis Santos <luis.jeronimo.s@gmail.com>
2023-06-17 18:55:26 +0100

    main update

--- space_jam/src/org/academiadecodigo/gamesweek/Main.java
+++ space_jam/src/org/academiadecodigo/gamesweek/Main.java
@@ -2,6 +2,10 @@ package org.academiadecodigo.gamesweek;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Game g = new Game(1217,720,200);
+
+        g.init();
+        g.start();
     }
 }
